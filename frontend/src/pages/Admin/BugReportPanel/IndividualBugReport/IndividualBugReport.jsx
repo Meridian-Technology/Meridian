7657cb9850f0ee18a92946330bfad920676dedb0
leej59 <diami5036@gmail.com>
2026-03-27 17:44:17 -0400

    MER-174 Added the individual bug report to the frontend

--- frontend/src/pages/Admin/BugReportPanel/IndividualBugReport/IndividualBugReport.jsx
+++ frontend/src/pages/Admin/BugReportPanel/IndividualBugReport/IndividualBugReport.jsx
@@ -4,13 +4,48 @@ import { useGradient } from '../../../../hooks/useGradient';
 import HeaderContainer from '../../../../components/HeaderContainer/HeaderContainer';
 import { Icon } from '@iconify-icon/react/dist/iconify.mjs';
 
-function IndividualBugReport() { 
+const bugData = {
+    "title": 'Dashboard not loading',
+    "bugNumber": '#BT-2041',
+    "priority": 'Critical',
+    "status": 'In Progress',
+    "tags": 'User Interface',
+    "assignee": 'James Liu',
+    "description": 'Dashboard does not load when I log into RPI email. It shows a blank white screen for me.',
+    "image": 'image'
+}
+
+function IndividualBugReport(title, bugNumber, priority, status, tags, assignee, description, image) { 
+    const { AdminGrad } = useGradient();
     return (
-        <section className="individual-bug-report dash">
+        <div className="individual-bug-report dash">
             <header className="header">
                 <h1>Bug Report</h1>
+                <img src={AdminGrad} alt="Admin Gradient" />
             </header>
-        </section>
+            <div className="report_details">
+                <h1>{bugData.title}</h1>
+                <div>
+                    <div className="report-meta">
+                        <div className="bug-number-priority-status">
+                            <h3>
+                                Bug Report {bugData.bugNumber}
+                            </h3>
+                            <h4 className={`priority-pill ${bugData.priority.toLowerCase()}`}>{bugData.priority}</h4>
+                            <h4 className="status-pill">{bugData.status}</h4>
+                        </div>
+                        <div className="bug-meta-row bug-meta-row--tags">
+                            <span className="tag-pill tag-pill--placeholder">Tags</span>
+                        </div>
+                        <div className="bug-meta-row bug-meta-row--assignee">
+                            <span className="assignee-pill assignee-pill--placeholder">Assignee: —</span>
+                        </div>
+                    </div>
+                    <p className="report-description">{bugData.description}</p>
+                    <h1>{bugData.image}</h1>
+                </div>
+            </div>
+        </div>
     )   
 }
 
